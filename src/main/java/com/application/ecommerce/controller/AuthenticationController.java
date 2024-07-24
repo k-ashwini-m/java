2cb791ddf535e6eb9ef54fc93820c34793f3bcfe
k-ashwini-m <157149998+k-ashwini-m@users.noreply.github.com>
2024-07-24 09:39:58 +0530

    Add files via upload

---
+++ src/main/java/com/application/ecommerce/controller/AuthenticationController.java
@@ -0,0 +1,49 @@
+package com.application.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.application.ecommerce.dto.JwtAuthenticationResponse;
+import com.application.ecommerce.dto.RefreshTokenRquest;
+import com.application.ecommerce.dto.SignInRequest;
+import com.application.ecommerce.dto.SingUpRequest;
+import com.application.ecommerce.entities.User;
+import com.application.ecommerce.service.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+	private final AuthenticationService authenticationService;
+	
+	@Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+	
+	@PostMapping("/singup")
+	public ResponseEntity<User> signup(@RequestBody SingUpRequest singUpRequest){
+		return ResponseEntity.ok(authenticationService.signup(singUpRequest));
+	}
+	
+	@PostMapping("/singin")
+	public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest singInRequest){
+		return ResponseEntity.ok(authenticationService.signin(singInRequest));
+	}
+	
+	@PostMapping("/refresh")
+	public ResponseEntity<JwtAuthenticationResponse> refresh(@RequestBody RefreshTokenRquest refreshTokenRquest){
+		return ResponseEntity.ok(authenticationService.refreshToken(refreshTokenRquest));
+	}
+	
+	@PostMapping("/adminsingup")
+	public ResponseEntity<User> adminsingup(@RequestBody SingUpRequest singUpRequest){
+		return ResponseEntity.ok(authenticationService.adminsingup(singUpRequest));
+	}
+}
